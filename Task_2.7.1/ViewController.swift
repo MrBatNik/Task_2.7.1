5edcda25102d7ab665c612c31b72c53e3ec17175
Nik Bat <niker85@bk.ru>
2021-11-09 15:42:42 +0300

    Initial Commit

---
+++ Task_2.7.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task_2.7.1
+//
+//  Created by Никита Бат on 09.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
